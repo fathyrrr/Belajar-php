f5a5ed6399e9356a0f713701a1969f93357d5caf
fathyrrr <muhamadfathir1501@gmail.com>
2026-04-14 23:50:53 +0800

    first commit belajar php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Budi";
+$umur = 20;
+$tinggi = 175.9;
+$hobi = array("membaca", "berenang", "bermain gitar");
+echo "Nama saya $nama, umur saya $umur tahun, tinggi saya $tinggi cm, dan hobi saya $hobi[1]"
+?>
