4f29698fa731a53e9ed5f7f278d3c98755f85d6c
fathyrrr <muhamadfathir1501@gmail.com>
2026-04-28 07:58:18 +0800

    menambahkan do while

--- materi2.php
+++ materi2.php
@@ -16,4 +16,33 @@ if(isset($_POST['angka'])){
 
 
 }
-    
+
+// Looping while dan do while
+
+echo "<br><b>while loop</b><br>";
+
+if (isset($_POST["angka"])) {
+    $data = $_POST["angka"];
+    $i = 1;
+    while ($i <= $data) {
+        // Cek ganjil genap
+        if ($i % 2 == 0) {
+            echo "nilai a adalah $i (Genap)<br>";
+        } else {
+            echo "nilai a adalah $i (Ganjil)<br>";
+        }
+        $i++;
+    }
+}
+
+echo "<br><br><b>do while loop</b><br>";
+$a = 10;
+do {
+    // Cek ganjil genap
+    if ($a % 2 == 0) {
+        echo "nilai a adalah $a (Genap)<br>";
+    } else {
+        echo "nilai a adalah $a (Ganjil)<br>";
+    }
+    $a--;
+} while ($a >= 5);
